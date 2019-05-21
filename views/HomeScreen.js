d68c96a06a83e65c03a07e5e79e2952bf1a83373
tomi <tomimustapha@icloud.com>
2019-05-21 08:16:30 -0700

    Add List Items

--- views/HomeScreen.js
+++ views/HomeScreen.js
@@ -1,7 +1,9 @@
 import React from "react";
-import { Button, StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, View, FlatList } from "react-native";
 import Roster from "./Roster";
 import Axios from "axios";
+import {ListItem}  from 'react-native-elements'
+
 
 class HomeScreen extends React.Component {
   static navigationOptions = {
@@ -9,11 +11,33 @@ class HomeScreen extends React.Component {
   };
   render() {
     const { navigate } = this.props.navigation;
+    const list = [
+      {
+        title: 'Houston Rockets',
+        avatar_url: 'https://i.pinimg.com/originals/da/db/db/dadbdb0c5775a80af7b9714a31eda1dd.jpg'
+      },
+      {
+        title: 'Toronto Raptors',
+        avatar_url: 'https://upload.wikimedia.org/wikipedia/en/thumb/3/36/Toronto_Raptors_logo.svg/1200px-Toronto_Raptors_logo.svg.png'
+      },
+    ]
+
+    
     return (
-      <Button
-        title="View Rockets Roster"
-        onPress={() => navigate("Roster", { title: "Rockets" })}
-      />
+      <View>
+      {
+        list.map((item, i) => (
+          <ListItem
+            key={i}
+            title={item.title}
+            leftAvatar={{ source:{uri: item.avatar_url } }}
+            onPress={() => navigate("Roster", { title: item.title + " Roster" })}
+            rightIcon={{ name: 'arrow-right', type: 'font-awesome'}}
+          />
+        ))
+      }
+      </View>
+
     );
   }
 }
